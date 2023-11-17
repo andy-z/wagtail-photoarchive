407f288b4af53e646e282295caa05f81b5550106
Andy Salnikov <andy@salnikov.us>
2023-11-17 11:28:00 -0800

    Update location of database and media files

--- photoarchive/settings/base.py
+++ photoarchive/settings/base.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = "photoarchive.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": os.path.join(BASE_DIR, "data", "db.sqlite3"),
     }
 }
 
@@ -145,7 +145,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesSto
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "data", "media")
 MEDIA_URL = "/media/"
 
 
