906f483b71fc9ce16ed983fdf73357dabb1075fc
Andy Salnikov <andy@salnikov.us>
2023-11-17 14:29:19 -0800

    try to fix logging

--- photoarchive/settings/production.py
+++ photoarchive/settings/production.py
@@ -2,6 +2,26 @@ from .base import *
 
 DEBUG = False
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        'logfile': {
+            'level':'DEBUG',
+            'class':'logging.FileHandler',
+            'filename': BASE_DIR + "/logfile.txt",
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console', 'logfile']
+    },
+}
+
 try:
     from .local import *
 except ImportError:
