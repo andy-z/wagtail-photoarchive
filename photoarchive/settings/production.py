5acf1a004417b2afec91c9f326f0525f6798c0f7
Andy Salnikov <andy@salnikov.us>
2023-11-17 18:29:30 -0800

    Update production config

--- photoarchive/settings/production.py
+++ photoarchive/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1", "minion", "minion.salnikov.us", "photo-archive.net", "www.photo-archive.net"
@@ -11,20 +11,33 @@ CSRF_TRUSTED_ORIGINS = ["https://photo-archive.net"]
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
     'handlers': {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+            "formatter": "verbose",
         },
         'logfile': {
             'level':'DEBUG',
             'class':'logging.FileHandler',
             'filename': BASE_DIR + "/logfile.txt",
+            "formatter": "verbose",
         },
     },
     'root': {
-        'level': 'DEBUG',
-        'handlers': ['console', 'logfile']
+        'level': 'INFO',
+        # 'handlers': ['console', 'logfile']
+        'handlers': ['console']
     },
 }
 
